8a6b8f3933f25c1824811eb12b598e3f79e4cba3
Alex Alvarez <a01207765@itesm.mx>
2018-03-09 20:33:45 +0000

    test

--- YunisCreativosBootStrapTestFakeNoOficial/registrar_actividad.php
+++ YunisCreativosBootStrapTestFakeNoOficial/registrar_actividad.php
@@ -9,33 +9,29 @@
       <label for="inputEmail4">Nombre Actividad</label>
       <input type="email" class="form-control" id="inputEmail4" placeholder="Nombre">
     </div>
-    <div class="form-group col-md-6">
-      <label for="inputPassword4">Password</label>
-      <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
-    </div>
   </div>
   <div class="form-group">
-    <label for="inputAddress">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+    <label for="inputAddress">Descripcion</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="Descripcion">
   </div>
   <div class="form-group">
-    <label for="inputAddress2">Address 2</label>
+    <label for="inputAddress2">Direccion 2</label>
     <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
   </div>
   <div class="form-row">
     <div class="form-group col-md-6">
-      <label for="inputCity">City</label>
+      <label for="inputCity">Ciudad</label>
       <input type="text" class="form-control" id="inputCity">
     </div>
     <div class="form-group col-md-4">
-      <label for="inputState">State</label>
+      <label for="inputState">Estado</label>
       <select id="inputState" class="form-control">
         <option selected>Choose...</option>
         <option>...</option>
       </select>
     </div>
     <div class="form-group col-md-2">
-      <label for="inputZip">Zip</label>
+      <label for="inputZip">C.P</label>
       <input type="text" class="form-control" id="inputZip">
     </div>
   </div>
