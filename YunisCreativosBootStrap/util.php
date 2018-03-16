4423b1512f1b9d24b3678beb30120507a51ea15b
Dante Flores <a01207543@itesm.mx>
2018-03-16 01:21:52 +0000

    Agregar login basado en roles

--- YunisCreativosBootStrap/util.php
+++ YunisCreativosBootStrap/util.php
@@ -13,4 +13,78 @@ function disconnectDB($mysql){
 }
 
 
+//FUNCION PARA VERIFICAR EL LOGIN
+function login($mail, $passwd) {
+  $db = connectDB();
+  if ($db != NULL) {
+
+    $query='SELECT correo FROM Usuario WHERE correo="'.$mail.
+        '" AND passwd="'.$passwd.'"';
+    
+    //Pa' debugear
+    //var_dump($query); 
+    //die('');
+    $results = $db->query($query);
+
+    if (mysqli_num_rows($results) > 0)  {
+        mysqli_free_result($results);
+        disconnectDB($db);
+        return true;
+    }
+    return false;
+  }
+  return false;
+}
+
+function getUserId($mail) {
+    $db = connectDB();
+    if ($db != NULL) {
+        
+        $query='SELECT idUsuario FROM Usuario WHERE correo="'.$mail.'"';
+        
+        //Pa' debugear
+        //var_dump($query); 
+        //die('');
+        $results = mysqli_query($db,$query);
+        disconnectDB($db);
+        
+        if (mysqli_num_rows($results) > 0) {
+            if($row = mysqli_fetch_assoc($results)) {
+                $res = "".$row["idUsuario"]."";
+            }else{
+                $res = "-1";
+            }
+        }
+        mysqli_free_result($results);
+    }
+    return $res;
+
+}
+
+//DEVUELVE EL ID DEL ROL DEL USUARIO EN UN ENTERO
+function getRol($idUser) {
+    $db = connectDB();
+    if ($db != NULL) {
+        
+        $query= 'SELECT R.idRol FROM Rol R, tiene T, Usuario U WHERE R.idRol=T.idRol AND U.idUsuario=T.idUsuario AND U.idUsuario='.$idUser;
+        
+        //Pa' debugear
+        //var_dump($query); 
+        //die('');
+        $results = mysqli_query($db,$query);
+        disconnectDB($db);
+        
+        if (mysqli_num_rows($results) > 0) {
+            if ($row = mysqli_fetch_assoc($results)){
+                $res = $row["idRol"];
+            }else{
+                $res = "-1";
+            }
+        }
+        mysqli_free_result($results);
+    }
+    return $res;
+}
+
+
 ?>
