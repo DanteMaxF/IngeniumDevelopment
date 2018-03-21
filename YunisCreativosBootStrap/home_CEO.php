a01d2beef61073e8c18a8fe9aa53a006ce31d14c
Dante Flores <a01207543@itesm.mx>
2018-03-21 16:10:03 +0000

    Arreglar home CEO

--- YunisCreativosBootStrap/home_CEO.php
+++ YunisCreativosBootStrap/home_CEO.php
@@ -1,22 +1,17 @@
 <?php 
 
-        include("partial/_navbarCEO.html");
-        require("FullCalendar/index.php");
-        include ("partial/_footer.html");
-        include("partial/_footer.html"); 
-    /*
     session_start();
     require_once("util.php");
     if( isset($_SESSION["idRol"]) ){
         include("partial/_head.html");
         include("partial/_navbarCEO.html");
         include("partial/_home_CEO.html"); 
+        require("FullCalendar/index.php");
         include("partial/_scripts.html");
         include("partial/_footer.html"); 
     }else{
         $_SESSION["errorLogin"] = "Es necesario iniciar sesión";
         header("location:index.php");
     }
-    */
 ?>
 
