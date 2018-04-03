f80912fb6ab022381fc7f1ff9835cae84cc1a718
Alex Alvarez <a01207765@itesm.mx>
2018-04-03 19:25:05 +0000

    consultar usuarios

--- YunisCreativosBootStrap/consulta_de_invitados.php
+++ YunisCreativosBootStrap/consulta_de_invitados.php
@@ -8,7 +8,7 @@
         }
         else{
             include("partial/_navbarCoordinador.html");
-        }
+        }-
         include("partial/_consulta_de_invitados.html");
         include("partial/_scripts.html");
         include("partial/_footer.html");
