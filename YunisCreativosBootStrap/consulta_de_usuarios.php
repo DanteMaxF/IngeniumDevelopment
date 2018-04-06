4b2384a712152077fb7686e8a58abc87830830e2
Alex Alvarez <a01207765@itesm.mx>
2018-04-06 01:29:00 +0000

    forma consulta usuario

--- YunisCreativosBootStrap/consulta_de_usuarios.php
+++ YunisCreativosBootStrap/consulta_de_usuarios.php
@@ -4,6 +4,14 @@
     if( isset($_SESSION["idRol"]) ){
         include("partial/_head.html");
         include("partial/_navbarCEO.html");
+         include("partial/_forma_consultar_usuario.html");
+        if($_GET["eventInput"] == "-" && $_GET["rolInput"] == "-"){
+            unset($_SESSION["evento"]);
+            unset($_SESSION["idEvento"]);
+            unset($_SESSION["rol"]);
+        }
+        
+        
         include("partial/_consulta_de_usuarios.html");
         include("partial/_footer.html");
         include("partial/_scripts.html");
