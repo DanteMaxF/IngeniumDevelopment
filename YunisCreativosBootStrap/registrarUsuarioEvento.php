2d71a946d79fea706374949036af6c887b6e359b
Alex Alvarez <a01207765@itesm.mx>
2018-05-08 01:21:24 +0000

    Registrar Invitado

--- YunisCreativosBootStrap/registrarUsuarioEvento.php
+++ YunisCreativosBootStrap/registrarUsuarioEvento.php
@@ -1,21 +1,35 @@
 <?php
     require_once("util.php");
     
-    $userId = getIDUserByMail($_GET['correo']);
+    $idInvitado = getIDUserByMail($_GET['correo']);
     $rol = $_GET["rol"];
+    $fechaNacimiento = $_GET["fechaNacimiento"];
+    $idEstado = $_GET["Estado"];
+    $talla = $_GET["talla"];
+    $idIdioma = $_GET["idioma"];
+    $alergias = $_GET["alergias"];
+    $mediacamentos = $_GET["medicamentos"];
     
-    
-    if (registrarRol($userId,$rol)){
-        header("location: index.php?success=1");
+
+     if (registrarRol($userId,$rol)){
+            if(($rol == 1496) || ($rol ==1495)){
+                if(registrarInvitado($idInvitado,$fechaNacimiento, $talla, $idEstado, $idIdioma,$alergias,$medicamentos)){
+                    header("location: index.php?success=1");
+                }
+            }
     }else{
         echo "ERROR";
     }
     
-    
+  
+   /* 
     //debugging...
-    echo "<br>";
-    echo 'User id: '.$userId;
-    echo "<br>";
-    echo 'rol: '.$rol;
-   
+    echo "<br> fecha nacimeinto: $fechaNacimiento";
+    echo "<br> estado: $idEstado";
+    echo "<br> talla: $talla";
+    echo "<br> idioma: $idIdioma";
+    echo "<br> alergias: $alergias";
+    echo "<br> medicamentos: $mediacamentos";
+    echo "<br> rol: $rol";
+    */
 ?>
