ef52f2cfc2dc1d8a0f0c8504d4d8380f0d20b1a2
Alex Alvarez <a01207765@itesm.mx>
2018-05-16 02:17:26 +0000

    Mi info: staff, admin, coordi

--- YunisCreativosBootStrap/foro_invitado.php
+++ YunisCreativosBootStrap/foro_invitado.php
@@ -2,20 +2,24 @@
      session_start();
     require_once("util.php");
     if( isset($_SESSION["idRol"]) ){
-        include("partial/_head.html");
         if($_SESSION["idRol"]== 1492){
+            include("partial/_head.html");
             include("partial/_navbarCEO.html");
         }
         else if($_SESSION["idRol"]== 1493){
+            include("partial/_head.html");
             include("partial/_navbarCoordinador.html");
         }
         else if($_SESSION["idRol"]== 1494){
+            include("partial/_head.html");
             include("partial/_navbarEmpleado.html");
         }
         else if($_SESSION["idRol"]== 1495){
+            include("partial/_head_invitado.html");
             include("partial/_navbarInvitado.html");
         }
         else if($_SESSION["idRol"]== 1496){
+            include("partial/_head_invitado.html");
             include("partial/_navbarCliente.html");
         }
         
---
+++ YunisCreativosBootStrap/mi_info_staff.php
@@ -0,0 +1,24 @@
+<?php 
+    session_start();
+    require_once("util.php");
+    $_SESSION["fEmepleado"] = 9999999;
+    if( isset($_SESSION["idRol"]) && (($_SESSION["idRol"]==1492) || ($_SESSION["idRol"]==1493) || ($_SESSION["idRol"]==1494 )) ){
+        include("partial/_head.html");
+         if($_SESSION["idRol"]== 1492){
+            include("partial/_navbarCEO.html");
+        }
+        else if($_SESSION["idRol"]== 1493){
+            include("partial/_navbarCoordinador.html");
+        }
+         else{
+            include("partial/_navbarEmpleado.html");
+        }
+        include("partial/_mi_info_staff.html");
+        include("partial/_scripts.html");
+        include("partial/_footer.html"); 
+        
+    }else{
+        $_SESSION["errorLogin"] = "Es necesario iniciar sesión";
+        header("location:index.php");
+    }
+?>
